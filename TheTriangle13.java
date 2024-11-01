c258b0cf0f0daa1cd35d46007f8cde9b64f0907b
fael83 <rafaellokajaya@gmail.com>
2024-11-01 13:34:27 +0700

    Lab 3

---
+++ TheTriangle13.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class TheTriangle13 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numInput , i = 0;
+        String s = " ";
+        System.out.print("Input some number: ");
+        numInput = input.nextInt();
+        while (i < numInput) {
+            s += " *";
+            System.out.println(s);
+            i++;
+        }
+    }
+}
