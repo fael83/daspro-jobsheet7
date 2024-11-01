21434c857977afe7f453e7284f189eac1b3726fc
fael83 <rafaellokajaya@gmail.com>
2024-11-01 13:25:51 +0700

    Lab 2, Question No. 1

--- DislayTwo13.java
+++ DislayTwo13.java
@@ -6,10 +6,10 @@ public class DislayTwo13 {
         int numInput;
         System.out.print("Input some number: ");
         numInput = input.nextInt();
-        for (int i = 1; i <= numInput; i++) {
-            if (i % 2 == 0) {
+        for (int i = 2; i <= numInput; i += 2) {
+            // if (i % 2 == 0) {
                 System.out.println("2 multiple: " + i);
-            }
+            
         }
     }
 }
